f54dc44c99914add18411c087a8b5abb326c35af
rachmann0 <reecedarby1889@gmail.com>
2021-11-11 21:23:34 +0700

    add package-lock.json to gitignore

--- src/redux/actions/activityActions.js
+++ src/redux/actions/activityActions.js
@@ -1,8 +1,12 @@
 import { actionTypes } from './../constants/action-types';
 
 export const setActivities = (activities) => {
-  return {
-    type: actionTypes.SET_ACTIVITIES,
-    payload: activities,
+  return (dispatch, getState, extraArgument) => {
+    // console.log(getState())
+    // console.log(extraArgument)
+    dispatch({
+      type: actionTypes.SET_ACTIVITIES,
+      payload: activities,
+    });
   };
 };
