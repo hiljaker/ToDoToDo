5cfb86568130c4c943d6f14724ee98fd2027c120
rachmann0 <reecedarby1889@gmail.com>
2021-11-12 14:55:55 +0700

    npm i react-vertical-timeline-component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,43 +1,127 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 //? css
 // import './App.css';
+import 'react-vertical-timeline-component/style.min.css';
 //? components
 // import Navbar from './components/Navbar';
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
 //? redux
-import { setActivities } from '../redux/actions/activityActions';
+import { getActivities } from '../redux/actions/activityActions';
 import { useDispatch, useSelector } from 'react-redux';
-//? hooks
-import { useDebounce } from '../hooks';
 
 export default function Home() {
-  const [state, setstate] = useState(0);
-
+  const activities = useSelector((state) => state.activityReducer.activities);
   const dispatch = useDispatch();
-  // const activities = useSelector((state) => state.activityReducer.activities);
 
-  // console.log(activities);
-
-  const onTestClick = useDebounce(
-    (e) => {
-      console.log(e.target);
-      // dispatch(
-      //   setActivities([
-      //     { activity_name: 'activity1' },
-      //     { activity_name: 'activity2' },
-      //     { activity_name: 'activity3' },
-      //   ])
-      // );
-      console.log('debounce test');
-      setstate(state + 1); // * affects
-    },
-    1000,
-    true
-  );
+  console.log(activities);
+  useEffect(() => {
+    dispatch(getActivities());
+  }, [dispatch]);
 
   return (
     <>
-      <h1>Home</h1>
-      <button onClick={onTestClick}>test redux action</button>
+      <VerticalTimeline>
+        <VerticalTimelineElement
+          className='vertical-timeline-element--work'
+          contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+          date='2011 - present'
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          // icon={<WorkIcon />}
+        >
+          <h3 className='vertical-timeline-element-title'>Creative Director</h3>
+          <h4 className='vertical-timeline-element-subtitle'>Miami, FL</h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, Project
+            Management, Team Leading
+          </p>
+          {/* <img src={}></img> */}
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className='vertical-timeline-element--work'
+          date='2010 - 2011'
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          // icon={<WorkIcon />}
+        >
+          <h3 className='vertical-timeline-element-title'>Art Director</h3>
+          <h4 className='vertical-timeline-element-subtitle'>
+            San Francisco, CA
+          </h4>
+          <p>
+            Creative Direction, User Experience, Visual Design, SEO, Online
+            Marketing
+          </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className='vertical-timeline-element--work'
+          date='2008 - 2010'
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          // icon={<WorkIcon />}
+        >
+          <h3 className='vertical-timeline-element-title'>Web Designer</h3>
+          <h4 className='vertical-timeline-element-subtitle'>
+            Los Angeles, CA
+          </h4>
+          <p>User Experience, Visual Design</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className='vertical-timeline-element--work'
+          date='2006 - 2008'
+          iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+          // icon={<WorkIcon />}
+        >
+          <h3 className='vertical-timeline-element-title'>Web Designer</h3>
+          <h4 className='vertical-timeline-element-subtitle'>
+            San Francisco, CA
+          </h4>
+          <p>User Experience, Visual Design</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className='vertical-timeline-element--education'
+          date='April 2013'
+          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+          // icon={<SchoolIcon />}
+        >
+          <h3 className='vertical-timeline-element-title'>
+            Content Marketing for Web, Mobile and Social Media
+          </h3>
+          <h4 className='vertical-timeline-element-subtitle'>Online Course</h4>
+          <p>Strategy, Social Media</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className='vertical-timeline-element--education'
+          date='November 2012'
+          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+          // icon={<SchoolIcon />}
+        >
+          <h3 className='vertical-timeline-element-title'>
+            Agile Development Scrum Master
+          </h3>
+          <h4 className='vertical-timeline-element-subtitle'>Certification</h4>
+          <p>Creative Direction, User Experience, Visual Design</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          className='vertical-timeline-element--education'
+          date='2002 - 2006'
+          iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+          // icon={<SchoolIcon />}
+        >
+          <h3 className='vertical-timeline-element-title'>
+            Bachelor of Science in Interactive Digital Media Visual Imaging
+          </h3>
+          <h4 className='vertical-timeline-element-subtitle'>
+            Bachelor Degree
+          </h4>
+          <p>Creative Direction, Visual Design</p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+          iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+          // icon={<StarIcon />}
+        />
+      </VerticalTimeline>
     </>
   );
 }
--- src/redux/actions/activityActions.js
+++ src/redux/actions/activityActions.js
@@ -10,3 +10,18 @@ export const setActivities = (activities) => {
     });
   };
 };
+
+export const getActivities = () => {
+  return async (dispatch, getState, api) => {
+    // api.get('/');
+    const activities = [
+      { activity_name: 'ngoding' },
+      { activity_name: 'belajar' },
+      { activity_name: 'blablabla' },
+    ];
+    dispatch({
+      type: actionTypes.SET_ACTIVITIES,
+      payload: activities,
+    });
+  };
+};
